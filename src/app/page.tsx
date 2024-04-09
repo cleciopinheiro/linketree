553bc8b2e4854f6f81bb3ad4dc05a3c456e382f1
cleciopinheiro <clecio.pinheiro@outlook.com>
2024-04-08 23:42:30 -0300

    Finalizando aplicação teste

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { GrCatalog } from "react-icons/gr";
+import { FaWhatsapp } from "react-icons/fa6";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="flex flex-col gap-8 px-12">
+        <Image src="/logomarca.png" alt="image" width={340} height={340} />
+        <div className="flex flex-col gap-4">
+          <a href="https://wa.me/5521965692272" target="_blank" className="flex py-3 px-6 bg-blue-800 gap-4 items-center justify-center text-white rounded-lg">
+            <FaWhatsapp size={32} />
+            WHATSAPP 1
+          </a>
+
+          <a href="https://wa.me/5521996684256" target="_blank" className="flex py-3 px-6 bg-blue-800 gap-4 items-center justify-center text-white rounded-lg">
+            <FaWhatsapp size={32} />
+            WHATSAPP 2
+          </a>
+          
+          <a href="https://drive.google.com/file/d/1a_WrN6PUBqMyNQcE7tDoknq-xke6TzEK/view?usp=sharing" target="_blank" className="flex py-3 px-6 bg-red-600 gap-4 items-center justify-center text-white rounded-lg">
+            <GrCatalog size={30} />
+            CATÁLOGO
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      'sm': {'min': '320px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+};
+export default config;
